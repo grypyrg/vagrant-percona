73706dd13288fb4cc5689eca9925c65a2ff854d9
drmac <github@matthewboehm.com>
2017-04-12 13:14:59 -0500

    formattting. readmes.

--- lib/vagrant-common.rb
+++ lib/vagrant-common.rb
@@ -10,7 +10,6 @@ def provider_aws( name, config, instance_type, region = nil, security_groups = n
 	require 'yaml'
 
 	aws_secrets_file = File.join( Dir.home, '.aws_secrets' )
-	
 	if( File.readable?( aws_secrets_file ))
 		config.vm.provider "aws" do |aws, override|
 			aws.instance_type = instance_type
@@ -25,7 +24,6 @@ def provider_aws( name, config, instance_type, region = nil, security_groups = n
 
 			# Used_subnet_id can be overridden if it is nil
 			used_subnet_id = subnet_id
-
 
 			# workaround for https://github.com/mitchellh/vagrant-aws/issues/331
 			override.vm.synced_folder ".", "/vagrant", type: "rsync"
@@ -46,7 +44,7 @@ def provider_aws( name, config, instance_type, region = nil, security_groups = n
 					used_subnet_id = aws_config['regions'][region]["default_vpc_subnet_id"]
 				end
 			else
-				puts "Warning: AWS region #{region} not defined in your ~/.aws_secrets file."
+				STDERR.puts "Warning: AWS region #{region} not defined in your ~/.aws_secrets file."
 			end
 
 			if used_subnet_id != nil
@@ -54,13 +52,13 @@ def provider_aws( name, config, instance_type, region = nil, security_groups = n
 				aws.subnet_id = used_subnet_id
 				aws.associate_public_ip = true
 			end
-		
+
 			if security_groups != nil
 				aws.security_groups = security_groups
 			end
-			
+
 			if Vagrant.has_plugin?("vagrant-hostmanager")
-				
+
 				if hostmanager_aws_ips == "private" or hostmanager_aws_ips == nil
 					awsrequest = "local-ipv4"
 				elsif hostmanager_aws_ips == "public"
@@ -77,12 +75,12 @@ def provider_aws( name, config, instance_type, region = nil, security_groups = n
 				end
 			end
 
-      if block_given?
-			  yield( aws, override )
-      end
+			if block_given?
+				yield( aws, override )
+			end
 		end
 	else
-		puts "Skipping AWS because of missing/non-readable #{aws_secrets_file} file.  Read https://github.com/jayjanssen/vagrant-percona/blob/master/README.md#aws-setup for more information about setting up AWS."
+		STDERR.puts "Skipping AWS because of missing/non-readable #{aws_secrets_file} file. Read https://github.com/percona/xtradb-cluster-tutorial/blob/master/README_AWS.md for more information about setting up AWS."
 	end
 end
 
@@ -94,7 +92,6 @@ def provider_virtualbox ( name, config, ram = 256, cpus = 1 )
 		vb.name = name
 		vb.cpus = cpus
 		vb.memory = ram
-
 		vb.customize ["modifyvm", :id, "--ioapic", "on" ]
 
 		# fix for slow dns https://github.com/mitchellh/vagrant/issues/1172
@@ -103,15 +100,15 @@ def provider_virtualbox ( name, config, ram = 256, cpus = 1 )
 
 		# Custom ip resolver that works with DHCP or explicit addresses (and is fast)
 		override.hostmanager.ip_resolver = proc do |vm, resolving_vm|
-		  if vm.id
-		    `VBoxManage guestproperty get #{vm.id} "/VirtualBox/GuestInfo/Net/1/V4/IP"`.split()[1]
-		  end
+			if vm.id
+				`VBoxManage guestproperty get #{vm.id} "/VirtualBox/GuestInfo/Net/1/V4/IP"`.split()[1]
+			end
 		end
 
 		if block_given?
-		  yield( vb, override )
+			yield( vb, override )
 		end
-	end	
+	end
 end
 
 # Configure this node for VMware
@@ -119,47 +116,44 @@ end
 # -- ram: amount of RAM (in MB)
 def provider_vmware ( name, config, ram = 256, cpus = 1 )
 	config.vm.provider "vmware_fusion" do |v, override|
-	    v.name = name
-	    v.vmx["memsize"] = ram
-	    v.vmx["numvcpus"] = cpus
+		v.name = name
+		v.vmx["memsize"] = ram
+		v.vmx["numvcpus"] = cpus
 
-	    if block_given?
-	      yield( v, override )
-	    end
-	end	
+		if block_given?
+			yield( v, override )
+		end
+	end
 end
 
 # Configure this node for Openstack
 def provider_openstack( name, config, flavor, security_groups = nil, network = nil, hostmanager_openstack_ips = nil )
-    require 'yaml'
-    require 'vagrant-openstack-plugin'
+	require 'yaml'
+	require 'vagrant-openstack-plugin'
 
-    os_secrets_file = File.join( Dir.home, '.openstack_secrets' )
+	os_secrets_file = File.join( Dir.home, '.openstack_secrets' )
 
-    if( File.readable?( os_secrets_file ))
-        config.vm.provider :openstack do |os, override|
-            os.flavor = flavor
+	if( File.readable?( os_secrets_file ))
+		config.vm.provider :openstack do |os, override|
+			os.flavor = flavor
+			os_config = YAML::load_file( os_secrets_file )
+			os.endpoint = os_config.fetch("endpoint")
+			os.username = os_config.fetch("username")
+			os.api_key = os_config.fetch("password")
+			os.tenant= os_config.fetch("tenant")
+			os.keypair_name = os_config.fetch("keypair_name")
+			override.ssh.private_key_path = os_config.fetch("private_key_path")
 
-            os_config = YAML::load_file( os_secrets_file )
+			if security_groups != nil
+				os.security_groups = security_groups
+			end
 
-            os.endpoint = os_config.fetch("endpoint")
-            os.username = os_config.fetch("username")
-            os.api_key = os_config.fetch("password")
-            os.tenant= os_config.fetch("tenant")
+			if network != nil
+				os.network = network
+			end
 
-            os.keypair_name = os_config.fetch("keypair_name")
-            override.ssh.private_key_path = os_config.fetch("private_key_path")
-
-            if security_groups != nil
-                os.security_groups = security_groups
-            end
-
-            if network != nil
-                os.network = network
-            end
-
-            os.floating_ip = :auto
-            os.floating_ip_pool = "external-net"
+			os.floating_ip = :auto
+			os.floating_ip_pool = "external-net"
 
 			if Vagrant.has_plugin?("vagrant-hostmanager")
 				if hostmanager_openstack_ips == "private" or hostmanager_openstack_ips == nil
@@ -178,13 +172,13 @@ def provider_openstack( name, config, flavor, security_groups = nil, network = n
 				end
 			end
 
-            if block_given?
-                yield( os, override )
-            end
-        end
-    else
-        puts "Skipping Openstack because of missing/non-readable #{os_secrets_file} file.  Read https://github.com/jayjanssen/vagrant-percona/blob/master/README.md#os-setup for more information about setting up Openstack."
-    end
+			if block_given?
+				yield( os, override )
+			end
+		end
+	else
+		STDERR.puts "Skipping Openstack because of missing/non-readable #{os_secrets_file} file. Read https://github.com/percona/xtradb-cluster-tutorial/blob/master/README_OPENSTACK.md for more information about setting up Openstack."
+	end
 end
 
 # Provision this node with Puppet
@@ -205,6 +199,5 @@ def provision_puppet( config, manifest_file )
 		if config.vm.hostname.is_a?(String)
 			puppet.facter["vagrant_hostname"] = config.vm.hostname
 		end
-
 	end
 end
